13e7a402d7b4cbc24f903965b1ed6e737f2f3368
TranLam2004 <142682958+TranLam2004@users.noreply.github.com>
2024-07-12 22:52:21 +0700

    123

--- dist/index.js
+++ dist/index.js
@@ -27764,10 +27764,7 @@ function wrappy (fn, cb) {
 /***/ 9739:
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
-//module.exports = require(__nccwpck_require__.ab + "build/Release/canvas.node")
-const canvasModulePath = `${__nccwpck_require__.ab}build/Release/canvas.node`;
-
-export default await import(canvasModulePath);
+module.exports = require(__nccwpck_require__.ab + "build/Release/canvas.node")
 
 /***/ }),
 
